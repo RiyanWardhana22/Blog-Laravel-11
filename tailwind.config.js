18accb610df50bb1d5863508b41fa815125eb8c5
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-03 23:53:13 +0700

    add: font interVariable (4. 14:03)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
