ee201485d511d01a883d1abc85dfc2c0399ec51c
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-06 23:20:08 +0700

    add: article dummy (6. 6:32)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,15 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
   <h1 class="text-xl">Hello World Guys</h1>
+
+  <article>
+    <h2>Sang Pemuja Rahasia</h2>
+    <div>
+      <a href="#">Riyan Wardhana</a> | 6 Mei 2025
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
+      error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?</p>
+    <a href="">Read more &raquo;</a>
+  </article>
 </x-layout>
