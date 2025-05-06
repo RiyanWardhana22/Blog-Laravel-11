7d95a2e9d540bc1aeabf3f5d30d10466f78ae694
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-06 23:27:23 +0700

    fix: styling article (6. 8:43)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,11 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  <h1 class="text-xl">Hello World Guys</h1>
-
-  <article>
-    <h2>Sang Pemuja Rahasia</h2>
-    <div>
+  <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Sang Pemuja Rahasia</h2>
+    <div class="text-base text-gray-500">
       <a href="#">Riyan Wardhana</a> | 6 Mei 2025
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    <p class="my-4 font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
       Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
       error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?</p>
     <a href="">Read more &raquo;</a>
