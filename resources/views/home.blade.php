0167e1ce24b513cecb0e36f6fddd35d12fda3d1c
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-06 21:50:47 +0700

    add: dynamic header (5. 9:35)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,7 @@
 <body class="h-full">
             <div class="min-h-full">
               <x-navbar></x-navbar>
-            <header class="bg-white shadow-sm">
-              <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
-              </div>
-            </header>
+              <x-header>Home Page</x-header>
             <main>
               <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
