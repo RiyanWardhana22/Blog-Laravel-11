2c75b4cd24e30ff47cb6a8a9a64601fb3032c8e0
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-07 10:46:47 +0700

    change: rename file blog to posts (6. 12:15)

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Sang Pemuja Rahasia</h2>
+    <div class="text-base text-gray-500">
+      <a href="#">Riyan Wardhana</a> | 6 Mei 2025
+    </div>
+    <p class="my-4 font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
+      error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?</p>
+    <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+  </article>
+
+  <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Sang Pengagum Rahasia</h2>
+    <div class="text-base text-gray-500">
+      <a href="#">Riyan Wardhana</a> | 6 April 2025
+    </div>
+    <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Repudiandae, repellendus? Ducimus ea expedita perferendis dicta inventore voluptates ipsum, 
+      explicabo ex atque impedit obcaecati quasi! Ex architecto repellendus fugiat distinctio suscipit.</p>
+    <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+  </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,23 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page', 'nama' => 'Riyan Wardhana']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'title' => 'Sang Pemuja Rahasia',
+            'author' => 'Riyan Wardhana',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
+      error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?'
+        ],
+        [
+            'title' => 'Sang Pengagum Rahasia',
+            'author' => 'Riyan Wardhana',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Repudiandae, repellendus? Ducimus ea expedita perferendis dicta inventore voluptates ipsum, 
+      explicabo ex atque impedit obcaecati quasi! Ex architecto repellendus fugiat distinctio suscipit.'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
