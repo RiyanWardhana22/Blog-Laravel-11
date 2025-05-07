bbb6b91dcf61c3776f5314baef3e4eca03c190c6
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-07 15:19:25 +0700

    fix: Move the articles array into models (7. 12:00)

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,7 @@
 <?php
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Post{
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'Sang-Pemuja-Rahasia',
-                'title' => 'Sang Pemuja Rahasia',
-                'author' => 'Riyan Wardhana',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-          Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
-          error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'Sang-Pengagum-Rahasia',
-                'title' => 'Sang Pengagum Rahasia',
-                'author' => 'Riyan Wardhana',
-                'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-          Repudiandae, repellendus? Ducimus ea expedita perferendis dicta inventore voluptates ipsum, 
-          explicabo ex atque impedit obcaecati quasi! Ex architecto repellendus fugiat distinctio suscipit.'
-            ]
-            ];
-    }
-}
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
