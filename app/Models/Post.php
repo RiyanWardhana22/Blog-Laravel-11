959ed58adb563981c8ff152ad05b7bc81f8ae301
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-10 13:24:04 +0700

    add: customizing the key (8. 14:10)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,39 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
-{
-            public static function all()
-            {
-                        return [
-                                    [
-                                                'id' => 1,
-                                                'slug' => 'Sang-Pemuja-Rahasia',
-                                                'title' => 'Sang Pemuja Rahasia',
-                                                'author' => 'Riyan Wardhana',
-                                                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Nobis, fugit unde? A, ullam ad. Quasi, cupiditate! Cumque incidunt tenetur voluptatem eius, 
-                        error earum blanditiis odit minima aspernatur, ducimus, nam consectetur?'
-                                    ],
-                                    [
-                                                'id' => 2,
-                                                'slug' => 'Sang-Pengagum-Rahasia',
-                                                'title' => 'Sang Pengagum Rahasia',
-                                                'author' => 'Riyan Wardhana',
-                                                'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                        Repudiandae, repellendus? Ducimus ea expedita perferendis dicta inventore voluptates ipsum, 
-                        explicabo ex atque impedit obcaecati quasi! Ex architecto repellendus fugiat distinctio suscipit.'
-                                    ]
-                        ];
-            }
-
-            public static function find($slug): array
-            {
-                        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-                        if (!$post) {
-                                    abort(404);
-                        }
-                        return $post;
-            }
-}
+class Post extends Model {}
