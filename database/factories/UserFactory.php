22f7a570c8a6234c2d3324d2ae05dc45e3672a4c
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-18 22:26:04 +0700

    add: method admin on table user (10. 16:00)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -45,7 +45,7 @@ class UserFactory extends Factory
     public function admin(): static
     {
         return $this->state(fn(array $attributes) => [
-            'admin' => true,
+            'is_admin' => true,
         ]);
     }
 }
