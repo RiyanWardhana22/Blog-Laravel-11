13f658a5f684bee1250c37a85a6a9c9f5558e129
riyanwardhana <riyanwardhana2@gmail.com>
2025-05-24 00:35:36 +0700

    fix: import models user

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Support\Str;
-use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
